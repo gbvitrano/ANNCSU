b0fd25eac912461a9c9f8c0a0335565fa75371ca
gbvitrano <gbvitrano@gmail.com>
2026-03-31 23:15:46 +0200

    upgrade

--- js/anncsu.js
+++ js/anncsu.js
@@ -2,6 +2,7 @@
   const PMTILES_URL = 'https://media.githubusercontent.com/media/anncsu-open/anncsu-viewer/main/data/anncsu-indirizzi.pmtiles';
   const MAP_CENTER  = [12.5, 42.0];
   const MAP_ZOOM    = 6;
+  const ITALY_BOUNDS = [[6.0, 35.5], [19.2, 47.8]]; // bounds leggermente più grandi dell'Italia
   const COLOR_OK    = '#4c9b82';
   const COLOR_ERR   = '#E63946';
   const DENOMI_PALETTE = [
@@ -857,12 +858,27 @@ style: {
     zoom: MAP_ZOOM,
     minZoom: 4,
     maxZoom: 19,
+    maxBounds: [[4.5, 33.5], [20.5, 49.5]], // bounds max leggermente più grandi dell'Italia
     hash: true,
 	  dragRotate: false,       // ← blocca rotazione con mouse
   pitchWithRotate: false   // ← blocca anche l'inclinazione (pitch)
   });
 
-  map.addControl(new maplibregl.NavigationControl(), 'top-right');
+  function resetToItaly() {
+    map.fitBounds(ITALY_BOUNDS, { padding: 20, duration: 800 });
+  }
+
+  const navControl = new maplibregl.NavigationControl({ showCompass: false });
+  map.addControl(navControl, 'top-right');
+  // Inserisce il pulsante Home tra zoom-in e zoom-out
+  const homeBtn = document.createElement('button');
+  homeBtn.type = 'button';
+  homeBtn.className = 'maplibregl-ctrl-home';
+  homeBtn.title = 'Vista Italia completa';
+  homeBtn.innerHTML = `<svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/><polyline points="9 22 9 12 15 12 15 22"/></svg>`;
+  homeBtn.onclick = resetToItaly;
+  const zoomOut = navControl._container.querySelector('.maplibregl-ctrl-zoom-out');
+  navControl._container.insertBefore(homeBtn, zoomOut);
   map.addControl(new maplibregl.ScaleControl({ unit: 'metric' }), 'bottom-right');
   map.addControl(new maplibregl.FullscreenControl(), 'top-right');
 
