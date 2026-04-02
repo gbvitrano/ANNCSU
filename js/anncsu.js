94fd48d8d0a90b4e09e0032a84575d5b18ea5325
gbvitrano <gbvitrano@gmail.com>
2026-04-02 10:51:33 +0200

    upgrade

--- js/anncsu.js
+++ js/anncsu.js
@@ -1242,7 +1242,8 @@ style: {
       this._btn = document.createElement('button');
       this._btn.type = 'button';
       this._btn.title = 'Mostra/nascondi comuni finanziati ANNCSU';
-      this._btn.innerHTML = `<svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="3 6 9 3 15 6 21 3 21 18 15 21 9 18 3 21"/><line x1="9" y1="3" x2="9" y2="18"/><line x1="15" y1="6" x2="15" y2="21"/></svg>`;
+      this._btn.className += ' comuni-ctrl-btn';
+      this._btn.innerHTML = `<span class="italia-icon"></span>`;
       // starts inactive (layer off by default)
       this._btn.onclick = () => {
         toggleComuniLayer();
